36f611345d960a9e7c98c0b2a518b995bb6f4a9e
Dmitry Kolesnichenko <39602114+zeter1@users.noreply.github.com>
2026-09-26 06:30:52 +0300

    test: decouple save boundary from clipboard layout

--- tests/raster-save-boundary.test.mjs
+++ tests/raster-save-boundary.test.mjs
@@ -6,7 +6,7 @@ import { createDocumentSessionController } from '../src/workspace/session-contro
 
 const main = await readFile(new URL('../src/main.js', import.meta.url), 'utf8');
 const boundary = main.slice(main.indexOf('function documentEditPending()'), main.indexOf('function reportRecoveryFailure('));
-const fileCommands = main.slice(main.indexOf('function saveProject()'), main.indexOf('function canvasToPngBlob('));
+const fileCommands = main.slice(main.indexOf('function saveProject()'), main.indexOf('async function prepareClearedHighDepthMutation'));
 const jumpHistory = main.slice(main.indexOf('function jumpToHistory('), main.indexOf('function updateLayerControls('));
 const fillCommand = main.slice(main.indexOf('async function fillAtPoint('), main.indexOf('async function clearSelectedPixels('));
 const deleteCommand = main.slice(main.indexOf('function deleteSelected()'), main.indexOf('function duplicateSelected()'));
